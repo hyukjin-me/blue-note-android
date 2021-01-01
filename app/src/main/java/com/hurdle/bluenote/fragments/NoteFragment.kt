83bb37e27e26d972794345da4f8cc5be126d17f8
hyukjin <hyukjin_89@naver.com>
2021-01-01 20:25:39 +0900

    빈 노트화면 추가

---
+++ app/src/main/java/com/hurdle/bluenote/fragments/NoteFragment.kt
@@ -0,0 +1,32 @@
+package com.hurdle.bluenote.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import com.hurdle.bluenote.databinding.FragmentHomeBinding
+import com.hurdle.bluenote.viewmodels.NoteViewModel
+
+class NoteFragment : Fragment() {
+
+    private lateinit var binding: FragmentHomeBinding
+
+    private lateinit var noteViewModel: NoteViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentHomeBinding.inflate(inflater)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        noteViewModel = ViewModelProvider(this).get(NoteViewModel::class.java)
+    }
+}
