4d9c63ff23f23b7f97746c38477f93ccd98c9eec
hyukjin <hyukjin_89@naver.com>
2021-01-06 17:00:00 +0900

    실시간 시간표시 기능 추가

--- app/src/main/java/com/hurdle/bluenote/fragments/NotePageFragment.kt
+++ app/src/main/java/com/hurdle/bluenote/fragments/NotePageFragment.kt
@@ -2,16 +2,22 @@ package com.hurdle.bluenote.fragments
 
 import android.content.Context
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.hurdle.bluenote.MainActivity
 import com.hurdle.bluenote.databinding.FragmentNotePageBinding
+import java.text.SimpleDateFormat
+import java.util.*
 
 class NotePageFragment : Fragment() {
 
     private lateinit var binding: FragmentNotePageBinding
+
+    val timeHandler = Handler(Looper.getMainLooper())
 
     override fun onAttach(context: Context) {
         super.onAttach(context)
@@ -32,5 +38,22 @@ class NotePageFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        displayOfTime()
+    }
+
+    private fun displayOfTime() {
+        // 실시간 시간표시
+        // https://stackoverflow.com/questions/55570990/kotlin-call-a-function-every-second
+        timeHandler.post(object : Runnable {
+            override fun run() {
+                val calendar = Calendar.getInstance()
+                val fullTimeFormat = SimpleDateFormat("yyyy-MM-dd (E) aa HH:mm:ss", Locale.getDefault())
+                val displayOfDate = fullTimeFormat.format(calendar.time)
+                binding.noteWordTimeTextView.text = displayOfDate
+
+                timeHandler.postDelayed(this, 1000)
+            }
+        })
     }
 }
