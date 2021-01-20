856f6aa80fd620fd22bcf8a280eb3607032c7ae5
hyukjin <hyukjin_89@naver.com>
2021-01-20 17:44:19 +0900

    헬퍼 빈 서비스클래스 추가

---
+++ app/src/main/java/com/hurdle/bluenote/service/SheetHelperService.kt
@@ -0,0 +1,9 @@
+package com.hurdle.bluenote.service
+
+import android.app.Service
+import android.content.Intent
+import android.os.IBinder
+
+class SheetHelperService : Service() {
+    override fun onBind(intent: Intent): IBinder? = null
+}
