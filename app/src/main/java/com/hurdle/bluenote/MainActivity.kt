aa270b33e9809d898f11a42df06648ffd1e3fa20
Hyukjin <hyukjin_89@naver.com>
2021-02-16 02:41:16 +0900

    앱종료기능 추가

--- app/src/main/java/com/hurdle/bluenote/MainActivity.kt
+++ app/src/main/java/com/hurdle/bluenote/MainActivity.kt
@@ -1,10 +1,12 @@
 package com.hurdle.bluenote
 
+import android.app.AlertDialog
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
 import android.view.View
+import android.widget.Button
 import android.widget.TextView
 import androidx.appcompat.widget.Toolbar
 import androidx.drawerlayout.widget.DrawerLayout
@@ -35,11 +37,14 @@ class MainActivity : AppCompatActivity() {
     private lateinit var sheetValueTextView: TextView
     private lateinit var sheetViewModel: SheetViewModel
 
+    private lateinit var finishAppButton: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         drawerLayout = findViewById(R.id.drawer_layout)
+        finishAppButton = findViewById(R.id.nav_main_finish_button)
 
         toolbar = findViewById(R.id.toolbar)
         setSupportActionBar(toolbar)
@@ -151,6 +156,18 @@ class MainActivity : AppCompatActivity() {
                     drawerLayout.setDrawerLockMode(DrawerLayout.LOCK_MODE_LOCKED_CLOSED)
                 }
             }
+        }
+
+        finishAppButton.setOnClickListener {
+            val builder = AlertDialog.Builder(this)
+            builder.setMessage(R.string.msg_finish_app)
+                .setPositiveButton(R.string.finish) { _, _ ->
+                    this.finish()
+                }
+                .setNegativeButton(R.string.cancel) { _, _ ->
+                }
+                .create()
+            builder.show()
         }
     }
 
